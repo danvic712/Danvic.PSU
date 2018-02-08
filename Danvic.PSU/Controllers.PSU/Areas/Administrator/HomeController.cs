e56b884b294518415393ff5558b445a189ec0e6b
Lanesra <danvic96@hotmail.com>
2018-02-08 21:19:53 +0800

    coding index page layout

---
+++ Danvic.PSU/Controllers.PSU/Areas/Administrator/HomeController.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file= "HomeController.cs">
+//     Copyright (c) Danvic712. All rights reserved.
+// </copyright>
+// Author: Danvic712
+// Date Created: 2018-02-08 19:35:43
+// Modified by:
+// Description: 
+//-----------------------------------------------------------------------
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controllers.PSU.Areas.Administrator
+{
+    [Area("Administrator")]
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
